effc6b1608e0b33d260db83faa78572e95c44462
Ty Eggen <tycho.eggen@gmail.com>
2024-09-21 16:56:08 +0000

    feat: various improvements and optimizations
    - formatting
    - literal string
    - html and css fixes
    - refactor qrcode options to config

--- app.js
+++ app.js
@@ -18,47 +18,39 @@ app.set('view engine', 'ejs')
 const config = require('./lib/config')
 
 app.get('/', function (req, res) {
-  res.redirect(config.wikiUrl)
+    res.redirect(config.wikiUrl)
 })
 
-app.get(
-  '/:page',
-  [check('page').isString().trim().escape()],
-  function (req, res) {
+app.get('/:page', [check('page').isString().trim().escape()], function (req, res) {
     const { page } = req.params
 
     if (page !== undefined && typeof page === 'string') {
-      res.redirect(config.wikiUrl + '?id=' + page)
-    } else { res.json({ result: 'ERROR', message: 'Missing or invalid page' }) }
-  }
-)
+        res.redirect(config.wikiUrl + '?id=' + page)
+    } else {
+        res.json({ result: 'ERROR', message: 'Missing or invalid page' })
+    }
+})
 
-app.get(
-  '/:page/qr',
-  [check('page').isString().trim().escape()],
-  function (req, res) {
+app.get('/:page/qr', [check('page').isString().trim().escape()], function (req, res) {
     const { page } = req.params
 
-    const { baseUrl, wikiUrl } = config
+    const { baseUrl, wikiUrl, qrCodeOptions } = config
 
     if (page !== undefined && typeof page === 'string') {
-      QRCode.toDataURL(
-        wikiUrl + '?id=' + page,
-        { size: 500, scale: 25, qzone: 5 },
-        function (err, qrImgData) {
-          if (err) {
-            console.log('error: ' + err)
-            res.json({
-              result: 'ERROR',
-              message: 'Error while generating QR code'
-            })
-          }
+        QRCode.toDataURL(`${wikiUrl}?id=${page}`, qrCodeOptions, (err, qrImgData) => {
+            if (err) {
+                console.log('error: ' + err)
+                res.json({
+                    result: 'ERROR',
+                    message: 'Error while generating QR code'
+                })
+            }
 
-          res.render('qrinfo', { baseUrl, wikiUrl, page, qrImgData })
-        }
-      )
-    } else { res.json({ result: 'ERROR', message: 'Missing or invalid page' }) }
-  }
-)
+            res.render('qrinfo', { baseUrl, wikiUrl, page, qrImgData })
+        })
+    } else {
+        res.json({ result: 'ERROR', message: 'Missing or invalid page' })
+    }
+})
 
 module.exports = app
